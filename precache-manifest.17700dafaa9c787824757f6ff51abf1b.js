ca22b155c8852abe26e34c128964c5f27fee0773
Przemocny <przemyslaw.jozwiakowski@gmail.com>
2019-12-03 17:22:21 +0100

    Updates

---
+++ precache-manifest.17700dafaa9c787824757f6ff51abf1b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2a56bb9bdf7d416303a8af81818089e",
+    "url": "/app-gr-11/index.html"
+  },
+  {
+    "revision": "364f48ad15668c4bf220",
+    "url": "/app-gr-11/static/css/main.55daa928.chunk.css"
+  },
+  {
+    "revision": "ef04d2cdfaa4a113e53c",
+    "url": "/app-gr-11/static/js/2.9caa780a.chunk.js"
+  },
+  {
+    "revision": "364f48ad15668c4bf220",
+    "url": "/app-gr-11/static/js/main.cc64b2a5.chunk.js"
+  },
+  {
+    "revision": "5f6b5e4284203c2ffccd",
+    "url": "/app-gr-11/static/js/runtime-main.f1183299.js"
+  }
+]);
