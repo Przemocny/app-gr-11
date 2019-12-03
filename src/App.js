5a8cdbfc0cb86bcc52380ef7bb8102a08d795cab
Przemocny <przemyslaw.jozwiakowski@gmail.com>
2019-12-03 18:06:43 +0100

    propsy i przekazywanie

--- src/App.js
+++ src/App.js
@@ -1,16 +1,69 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+
 import ReduxComponent from './stores/ReduxComponent'
 import './App.css'
 
+// redux - przypomnienie
+// prop-types - upewnianie się
+// deploy na gh-pages - https://typeofweb.com/react-js-na-github-pages-dzieki-create-react-app/
 
+const View = ({weatherData}) => {
+	return <div>View {JSON.stringify(weatherData)}</div>
+}
+
+const Form = ({onChange, onClick, value}) => {
+	return (
+		<div>
+			<input onChange={onChange} value={value}/>
+			<button onClick={onClick}>click</button>
+		</div>
+	)
+}
+
+class Wrapper extends React.Component {
+	render() {
+
+		const {appState, onInputChange, onSubmit} = this.props
+
+		return (
+			<div>
+				<div>
+					<Form onChange={onInputChange} onClick={onSubmit} value={appState.input} />
+					<View weatherData={appState.weatherData}/>
+				</div>
+			</div>
+		)
+	}
+}
 
 class App extends React.Component {
+
+	state = {input:'', weatherData:[]}
+
 	componentDidMount() {}
+	onInputChange = (ev)=>{
+		const {value} = ev.target
+		// tu się zmienia stan
+		console.log('someMethod',value)
+		this.setState({
+			input:value
+		})
+	}
+
+	onSubmit = ()=>{
+		console.log(this.state)
+	}
+
 
 	render() {
 		return (
 			<div className='App' style={{ margin: 30 }}>
-				<ReduxComponent/>
+				<Wrapper 
+					appState={this.state}
+					onInputChange={this.onInputChange}
+					onSubmit={this.onSubmit}
+		/>
 			</div>
 		)
 	}
